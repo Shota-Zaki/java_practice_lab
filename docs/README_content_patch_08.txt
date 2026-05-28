113c9f2a289c1a223ab1d9e186f2afe3b66f9e41
Shota-Zaki <shota690@gmail.com>
2026-05-28 12:29:46 +0900

    Delete docs directory

--- docs/README_content_patch_08.txt
+++
@@ -1,1 +0,0 @@
-追加コンテンツ差分08。Bronze / Java11 Silver / Gold の解説と記事を追加。docs/配下を上書きしてください。
--- docs/pwa.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator && location.protocol !== 'file:') {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').catch(() => {});
-  });
-}
