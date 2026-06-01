ad80ff1236a8dad0bde2d0e998b33e0d4afef6e7
Shota-Zaki <shota690@gmail.com>
2026-06-01 19:34:27 +0900

    Delete docs directory

--- docs/pwa.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  if (!('serviceWorker' in navigator) || location.protocol === 'file:') return;
-  window.addEventListener('load', () => {
-    const scriptUrl = document.currentScript && document.currentScript.src ? document.currentScript.src : new URL('pwa.js', location.href).href;
-    const swUrl = new URL('sw.js', scriptUrl).href;
-    navigator.serviceWorker.register(swUrl).catch(() => {});
-  });
-})();
